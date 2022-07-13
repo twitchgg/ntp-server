8a059d5d4059e9e0b65d1115aa956137431c8c48
TwitchGG <twitchgg@yahoo.com>
2022-07-14 01:08:18 +0800

    update

--- test/ntp_test.go
+++ test/ntp_test.go
@@ -3,6 +3,7 @@ package test
 import (
 	"fmt"
 	"testing"
+	"time"
 
 	"github.com/beevik/ntp"
 	ntsc "ntsc.ac.cn/ta/ntp-server/internal/ntp"
@@ -29,4 +30,13 @@ func TestNTP(t *testing.T) {
 		t.Fatal(err)
 	}
 	fmt.Println("offset:", response.ClockOffset)
+}
+
+func TestTimeFormat(t *testing.T) {
+	str := "2022-07-14T00:03:57.296714700+08:00"
+	s, err := time.Parse(time.RFC3339Nano, str)
+	if err != nil {
+		t.Fatal(err)
+	}
+	fmt.Println(s)
 }
